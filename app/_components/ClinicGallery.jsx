58ba688f40fdb176dfb8e7ac31c825d2219d1d57
herrbaha <baha_i@hotmail.com>
2024-08-14 14:26:22 +0200

    create clinic carousel and mock datas

---
+++ app/_components/ClinicGallery.jsx
@@ -0,0 +1,61 @@
+'use client'
+
+import React, { useCallback } from "react";
+import useEmblaCarousel from "embla-carousel-react";
+import Image from "next/image";
+import { data } from "../_mocks_/ClinicCarouselData";
+import { FaArrowLeft, FaArrowRight } from "react-icons/fa";
+
+const ClinicGallery = () => {
+  const [emblaRef, emblaApi] = useEmblaCarousel({ loop: true });
+
+  const scrollPrev = useCallback(() => {
+    if (emblaApi) emblaApi.scrollPrev();
+  }, [emblaApi]);
+
+  const scrollNext = useCallback(() => {
+    if (emblaApi) emblaApi.scrollNext();
+  }, [emblaApi]);
+
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <div className="relative w-[60%]">
+        <div className="overflow-hidden" ref={emblaRef}>
+          <div className="flex -ml-4">
+            {data.map((item) => (
+              <div key={item.id} className="min-w-[calc(100%/3)] pl-4">
+                <div className="relative">
+                  <Image
+                    src={item.image}
+                    alt={item.alt}
+                    title={item.title}
+                    width={800}
+                    height={500}
+                    className="rounded-lg object-cover h-[500px] w-full"
+                  />
+                  <div className="absolute bottom-0 w-full bg-black bg-opacity-50 text-white p-4 text-center rounded-b-lg">
+                    {item.title}
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+        <button
+          onClick={scrollPrev}
+          className="absolute top-1/2 left-0 transform -translate-y-1/2 bg-black bg-opacity-50 text-white p-3 rounded-full z-10 ml-2"
+        >
+          <FaArrowLeft size={20} />
+        </button>
+        <button
+          onClick={scrollNext}
+          className="absolute top-1/2 right-0 transform -translate-y-1/2 bg-black bg-opacity-50 text-white p-3 rounded-full z-10 mr-2"
+        >
+          <FaArrowRight size={20} />
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default ClinicGallery;
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,15 @@
-import Image from "next/image";
+import dynamic from "next/dynamic";
+
+const ClinicGallery = dynamic(() => import("./_components/ClinicGallery"), {
+  ssr: false,
+  loading: () => <p>Loading...</p>,
+});
 
 export default function Home() {
   return (
     <main>
-      Main Page
+      <h1>Main Page</h1>
+      <ClinicGallery />
     </main>
   );
 }
