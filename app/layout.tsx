acafd97731e7106f31ddecc5f42c5fd99fac9499
Semra Elif <121907413+elifalinli@users.noreply.github.com>
2024-08-15 11:11:24 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <Navbar/>
-        <div className="flex">
+        <div>
           <div className="w-full">{children}</div>
         </div>
       </body>
