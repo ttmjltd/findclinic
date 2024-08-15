24e3a5ac32754f4f8325a6be58ceea71384883bb
herrbaha <baha_i@hotmail.com>
2024-08-15 12:31:47 +0200

    fix conflict3

---
+++ app/_mocks_/ClinicCarouselData.js
@@ -0,0 +1,49 @@
+export const data = [
+  {
+    id: 1,
+    type: "image",
+    image: "/culture.webp",
+    alt: "Angel Health Tourism",
+    title: "Angel Health Tourism"
+  },
+  {
+    id: 2,
+    type: "image",
+    image: "/doctor-working.avif",
+    alt: "Mayo Health Clinic",
+    title: "Mayo Health Clinic",
+
+  },
+  {
+    id: 3,
+    type: "image",
+    image: "/health_consulting.jpg",
+    alt: "Grand Clinic Atlantis",
+    title: "Grand Clinic Atlantis",
+
+  },
+
+  {
+    id: 4,
+    type: "image",
+    image: "/mission.jpg",
+    alt: "Angel Health Tourism",
+    title: "Angel Health Tourism"
+  },
+  {
+    id: 5,
+    type: "image",
+    image: "/surgery.jpeg",
+    alt: "Mayo Health Clinic",
+    title: "Mayo Health Clinic",
+
+  },
+  {
+    id: 6,
+    type: "image",
+    image: "/woman-smiling.webp",
+    alt: "Grand Clinic Atlantis",
+    title: "Grand Clinic Atlantis",
+
+  },
+];
