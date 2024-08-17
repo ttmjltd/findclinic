b7bd256efcb22d8822a4fd4afcd7e76da1b4f7fc
mehmet ali <beyzadetoprak@gmail.com>
2024-08-17 21:31:43 +0200

    Explore other options implementation

--- app/_components/Navbar.tsx
+++ app/_components/Navbar.tsx
@@ -55,8 +55,8 @@ const Navbar: React.FC = () => {
               className="w-6 h-6"
             >
               <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
+                strokeLinecap="round"
+                strokeLinejoin="round"
                 d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
               ></path>
             </svg>
