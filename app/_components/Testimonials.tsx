a8f31eb1f2690812c9f656e95e4d2f7a6e8f82cd
Fidan429 <fidanyuksekkaya429@gmail.com>
2024-09-02 21:24:09 +0300

    testimonials3

--- app/_components/Testimonials.tsx
+++ app/_components/Testimonials.tsx
@@ -1,25 +1,21 @@
-import Image from 'next/image';
-import { comments } from '../_mocks_/TestimonialsData';
-
-
+import Image from "next/image";
+import { comments } from "../_mocks_/TestimonialsData";
 
 
 const Testimonials: React.FC = () => {
   return (
-<div className="container mx-auto py-8 ">
- <div className="w-[900px] h-[675px]  m-4 flex flex-col gap-2 pt-8 ">
-  <span className="text-2xl text-secondary text-center font-bold">
-  What our patients say
-  </span>
- 
- 
+    <div className="container mx-auto my-10 flex flex-col gap-10 content-center items-center w-3/6 p-6">
+      <span className="text-2xl text-secondary text-center font-bold">
+        What our patients say
+      </span>
 
-      {comments.map((comment, index) => (
+      {comments.map((comment,index) => (
         <div
           key={index}
-          className={`flex items-center gap-6 ${comment.reverse ? 'flex-row-reverse' : ''} flex-row`}
+          className={`flex items-center gap-6 ${
+            comment.reverse ? "flex-row-reverse" : ""
+          } flex-row`}
         >
-          
           <Image
             src={comment.image}
             alt={`Yorum ${index + 1}`}
@@ -27,15 +23,14 @@ const Testimonials: React.FC = () => {
             height={128}
             className="object-cover rounded-lg"
           />
-          
+
           <div>
-          <p className="text-gray-800 text-base">{comment.text}</p><br/>
-          <p className="text-gray-800 text-base">{comment.name}</p>
-           </div>
+            <p className="text-gray-800 text-base w-2/3 ">{comment.text}</p>
+            <br />
+            <p className="text-gray-800 text-base">{comment.name}</p>
+          </div>
         </div>
-       
       ))}
-    </div> 
     </div>
   );
 };
