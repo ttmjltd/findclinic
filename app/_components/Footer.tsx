fc24088f436619a424704961a14f07455761aae5
herrbaha <baha_i@hotmail.com>
2024-09-19 12:04:22 +0200

    fix mobile

--- app/_components/Footer.tsx
+++ app/_components/Footer.tsx
@@ -11,13 +11,13 @@ const footerLinks: { [key: string]: FooterLink } = {
     label: "About Us",
     href: "/about",
   },
-  terms: {
-    label: "Terms & Conditions",
-    href: "/terms",
-  },
   clinics: {
     label: "Clinics",
     href: "/clinics",
+  },
+  terms: {
+    label: "Terms & Conditions",
+    href: "/terms",
   },
   contact: {
     label: "Contact Us",
@@ -46,15 +46,15 @@ const footerLinks: { [key: string]: FooterLink } = {
 };
 const Footer: React.FC = () => {
   return (
-    <section className="container w-full mx-auto bg-white text-primary leading-4 border rounded-xl backdrop-blur-xl backdrop-saturate-200 mt-10 py-4 flex flex-col sm:flex-row justify-between items-start sm:w-2/3 sm:h-2/6">
-      <div className="flex flex-col sm:flex-row items-start w-full sm:w-full sm:justify-between">
-        <div className="flex sm:flex-col sm:items-start ml-6 mt-6 items-center">
+    <section className="md:container w-full mx-auto bg-white text-primary leading-4 border rounded-xl backdrop-blur-xl backdrop-saturate-200 mt-10 py-4 flex flex-col md:flex-row justify-between items-start md:w-2/3 md:h-2/6">
+      <div className="flex flex-col md:flex-row items-start w-full md:w-full md:justify-between">
+        <div className="flex md:flex-col md:items-start md:ml-6 mt-6 items-center md:w-1/4 w-5/6 mx-auto">
           <Image
             width={100}
             height={30}
             src="/Logo2findclinics.png"
             alt="find clinics logo small"
-            className="block sm:hidden w-2/5 h-auto mr-auto"
+            className="block md:hidden w-2/6 h-auto mr-auto"
           />
 
           <Image
@@ -62,18 +62,18 @@ const Footer: React.FC = () => {
             height={45}
             src="/Logo1findclinics.png"
             alt="find clinics logo"
-            className="hidden sm:block w-full h-auto"
+            className="hidden md:block w-full h-auto"
           />
-          <div className="sm:mt-10 sm:ml-0 ml-auto">
+          <div className="md:mt-10 md:ml-0 ml-auto">
             <p className="text-primary mb-4">info@healthway.co.uk</p>
             <p className="text-primary">+447564123765</p>
           </div>
         </div>
 
-        <div className="flex items-start ml-6 sm:w-3/4">
+        <div className="flex items-start ml-6 md:w-3/4">
           <div className="hidden md:block w-px h-36 border border-[#2A9D8F] m-4 my-6 mr-6"></div>
 
-          <div className="grid grid-cols-2 md:grid-cols-3 my-10 gap-2 w-full">
+          <div className="grid grid-cols-2 md:grid-cols-3 my-10 gap-2 md:w-full w-full">
             {Object.entries(footerLinks).map(([key, link]) => (
               <ul key={key} className="flex flex-col space-y-1">
                 <li>
@@ -90,28 +90,28 @@ const Footer: React.FC = () => {
         </div>
       </div>
 
-      <div className="flex sm:flex-col items-center md:flex-row absolute bottom-6 space-x-2 sm:right-10 right-3">
+      <div className="flex items-center md:flex-row md:absolute md:bottom-6 space-x-2 md:right-10 mx-auto">
         <Image src="/linkedin.png" alt="linkedin icon" width={24} height={24} />
         <Image
           src="/facebook.png"
           alt="facebook icon"
           width={24}
           height={24}
-          className="bg-[#2A9D8F] rounded-sm p-0.5"
+          className="bg-[#2A9D8F] rounded-md p-0.5"
         />
         <Image
           src="/instagram.png"
           alt="instagram icon"
           width={24}
           height={24}
-          className="bg-[#2A9D8F] rounded-sm p-1"
+          className="bg-[#2A9D8F] rounded-md p-1"
         />
         <Image
           src="/Youtube.png"
           alt="youtube icon"
           width={24}
           height={24}
-          className="bg-[#2A9D8F] rounded-sm p-1"
+          className="bg-[#2A9D8F] rounded-md p-1"
         />
       </div>
     </section>
