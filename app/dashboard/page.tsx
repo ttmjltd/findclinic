1a7b3a63a98b5c68dacac78ad3b8742f3dad327b
herrbaha <baha_i@hotmail.com>
2024-09-19 12:07:27 +0200

    solve rebase2

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -7,7 +7,6 @@ import { ProfileInfoValues } from "../types";
 import DashboardTreatments from "../_components/DashboardTreatments";
 import DashboardPrices from "../_components/DashboardPrices";
 import DashboardLocation from "../_components/DashboardLocation";
-import DashboardPrices from "../_components/DashboardPrices";
 
 const DashboardPage = () => {
   const handleSubmit = (values: ProfileInfoValues) => {
@@ -24,7 +23,6 @@ const DashboardPage = () => {
       <DashboardTreatments />
       <DashboardPrices />
       <DashboardLocation/>
-      <DashboardPrices />
     </div>
   );
 };
