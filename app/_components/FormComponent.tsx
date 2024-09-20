49672138e951ad4e427eef4f8d146db7a9204ff9
Saliha Oguz <118985690+salihaoguz11@users.noreply.github.com>
2024-09-20 17:44:49 +0100

    md screen updated

--- app/_components/FormComponent.tsx
+++ app/_components/FormComponent.tsx
@@ -21,7 +21,7 @@ const FormComponent = () => {
 
   return (
     <div className="h-full lg:flex lg:justify-center ">
-      <div className="lg:w-[900px] h-full  lg:h-[675px] bg-white rounded mx-4  flex flex-col gap-2 pt-8 px-4 lg:px-0  ">
+      <div className="lg:w-[900px] h-full  lg:h-[675px] bg-white rounded mx-4 md:mx-16 lg:mx-4 flex flex-col gap-2 pt-8 md:px-16  lg:px-0  ">
         <span className="text-2xl text-secondary text-center font-bold">
           Schedule a Call Today!
         </span>
@@ -63,19 +63,19 @@ const FormComponent = () => {
               checked={subscribe}
             />
           </div>
-          <div className=" mt-12 md:mt-8 flex justify-center lg:hidden">
+          <div className=" mt-12 md:mt-8 md:text-2xl flex justify-center lg:hidden">
             <Button label="Schedule a call now!" variation="broad-dark" />
           </div>
-          <div className="w-full  lg:w-[342px] relative mt-40 ">
-            <div className="bg-secondary  lg:w-[320px] rounded  h-[310px]  lg:absolute opacity-60   bottom-[145px] left-0"></div>
+          <div className="w-full  lg:w-[342px] relative mt-40  ">
+            <div className="bg-secondary  lg:w-[320px] rounded  h-[310px]   lg:absolute opacity-60   bottom-[145px] left-0 "></div>
             <Image
               src="/advisor.png"
               alt="advisor-image"
               width={350}
               height={400}
-              className="object-cover absolute  bottom-[145px] left-0"
+              className="object-cover absolute  bottom-[145px] left-0 md:left-24 lg:left-0"
             />
-            <div className="flex flex-col  left-0 lg:absolute lg:bottom-[70px] lg:left-14 text-center lg:text-lef  ">
+            <div className="flex flex-col  left-0 lg:absolute lg:bottom-[70px] lg:left-14 text-center lg:text-left  ">
               <span className="text-[32px] ">Jane Marson</span>
               <span className="text-[16px] ">Your personal health advisor</span>
             </div>
