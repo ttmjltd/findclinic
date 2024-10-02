6f066281d9fbdea67fe58b8671b762480aefe6d5
Elif Alinli <semraelifalinli@gmail.com>
2024-10-02 14:22:27 +0100

    change style in mobile and desktop

--- app/_components/OptionalCards.tsx
+++ app/_components/OptionalCards.tsx
@@ -5,20 +5,10 @@ import React, { useRef } from "react";
 import { data } from "../_mocks_/ClinicCarouselData";
 import { ClinicDataTypes } from "../types";
 import { FaArrowLeft, FaArrowRight } from "react-icons/fa";
+import { CompareButtonFC, LikeButtonFC, StarFC } from "../_atoms/Icons";
 
 const OptionalCards: React.FC = () => {
   const scrollRef = useRef<HTMLDivElement>(null);
-
-  const scroll = (direction: "left" | "right") => {
-    if (scrollRef.current) {
-      const { scrollLeft, clientWidth } = scrollRef.current;
-      const scrollTo =
-        direction === "left"
-          ? scrollLeft - clientWidth
-          : scrollLeft + clientWidth;
-      scrollRef.current.scrollTo({ left: scrollTo, behavior: "smooth" });
-    }
-  };
 
   return (
     <div className="container mx-auto py-8">
@@ -26,52 +16,50 @@ const OptionalCards: React.FC = () => {
         Explore Other Options
       </h2>
       <div className="relative">
-        <button
-          className="absolute top-1/2 left-0 transform -translate-y-1/2 bg-black bg-opacity-50 text-white p-3 rounded-full z-5 ml-4"
-          onClick={() => scroll("left")}
-        >
-          <FaArrowLeft size={20} />
-        </button>
         <div
           ref={scrollRef}
           className="flex overflow-x-scroll pb-10 hide-scroll-bar"
         >
-          <div className="flex flex-nowrap">
+          <div className="flex flex-nowrap pl-7">
             {data.map((item: ClinicDataTypes) => (
-              <div key={item.id} className="inline-block px-3">
-                <div className="w-64 h-72 max-w-xs overflow-hidden rounded-lg shadow-md bg-white hover:shadow-xl transition-shadow duration-300 ease-in-out">
-                  <Image
-                    src={item.image}
-                    alt={item.alt}
-                    className="w-full h-32 sm:h-48 object-cover"
-                    width={200}
-                    height={200}
-                  />
-                  <div className="p-1 text-center">
-                    <h3 className="mt-0.5 text-sm  font-medium text-blue-900">
+              <div key={item.id} className="inline-block px-2 ">
+                <div className="w-[275px] h-72 max-w-xs overflow-hidden rounded-lg shadow-md bg-white hover:shadow-xl transition-shadow duration-300 ease-in-out">
+                  <div>
+                  <div className="flex justify-center">
+                    <Image
+                      src={item.image}
+                      alt={item.alt}
+                      className=" w-full h-32 xs:h-40 object-cover m-3 rounded-lg"
+                      width={200}
+                      height={200}
+                    />
+                  </div>
+                  <div className="pl-3 py-4 space-y-2 flex justify-between items-center">
+                    <h3 className="mt-0.5 text-start text-base font-medium text-primary">
                       {item.title}
                     </h3>
+                    <div className="inline-flex space-x-2 pr-2">
+                      <CompareButtonFC />
+                      <LikeButtonFC />
+                    </div>
                   </div>
-                  <div className="m-3 flex flex-wrap gap-2">
-                    <span className="whitespace-nowrap rounded-full bg-blue-100 px-2.5 py-0.5 text-xs text-blue-600">
-                      Hair Transplant
+                  <div className="flex items-center pl-3 mb-3 lg:mt-1">
+                    <span className="text-2xl">
+                      <StarFC color="#2A9D8F" />
                     </span>
-
-                    <span className="whitespace-nowrap rounded-full bg-blue-100 px-2.5 py-0.5 text-xs text-blue-600">
-                      Dental Care
+                    <span className="mr-2 text-primary text-sm px-1">
+                      4.8{" "}
+                      <span className="ml-1 md:text-sm">
+                        (250 Reviews)
+                      </span>
                     </span>
                   </div>
+                </div>
                 </div>
               </div>
             ))}
           </div>
         </div>
-        <button
-          className="absolute top-1/2 right-0 transform -translate-y-1/2 bg-black bg-opacity-50 text-white p-3 rounded-full z-5 mr-2"
-          onClick={() => scroll("right")}
-        >
-          <FaArrowRight size={20} />
-        </button>
       </div>
     </div>
   );
