b0128ad4297435bb3ef80881ce07e5acb023086c
Elif Alinli <semraelifalinli@gmail.com>
2024-10-18 17:25:01 +0100

    changed colour

--- app/_atoms/BreadCrum.tsx
+++ app/_atoms/BreadCrum.tsx
@@ -4,9 +4,9 @@ import { BreadCrumTypes } from '../types'
 const BreadCrum: React.FC<BreadCrumTypes> = ({ clinicName }) => {
   return (
     <div className="flex flex-wrap text-sm md:text-base space-x-1 md:space-x-2">
-      <span>Home</span>
+      <span className='text-primary' >Home</span>
       <span className='text-secondary'> {">"} </span>
-      <span>Treatment</span>
+      <span className='text-primary'>Treatment</span>
       <span className='text-secondary'> {">"} </span>
       <span className='text-secondary'>{clinicName}</span>
     </div>
