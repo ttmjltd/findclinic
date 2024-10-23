593fdaeb92a0e4acd3913dc05236f4b15b4e41ed
herrbaha <baha_i@hotmail.com>
2024-10-23 19:56:37 +0200

    fix review

--- app/_components/DoctorsCard.tsx
+++ app/_components/DoctorsCard.tsx
@@ -9,30 +9,30 @@ const DoctorsCard: React.FC = () => {
         {doctorsData.map((item: DoctorDataTypes) => (
           <div
             key={item.id}
-            className="border border-brightNavy rounded-lg p-4 w-full text-center"
+            className="border border-brightNavy rounded-lg p-4 w-full text-start hover:bg-neutral hover:border-none"
           >
-              <div className="flex items-center justify-between">
-                <div className="w-3/6">
-                  <Image
-                    src={item.image}
-                    alt={item.alt}
-                    title={item.fullName}
-                    width={96}
-                    height={96}
-                    className="rounded-full object-cover"
-                  />
-                </div>
-                <div>
+            <div className="flex items-center justify-between">
+              <div className="w-3/6">
+                <Image
+                  src={item.image}
+                  alt={item.alt}
+                  title={item.fullName}
+                  width={96}
+                  height={96}
+                  className="rounded-full object-cover"
+                />
+              </div>
+              <div>
                 <h2 className="font-semibold text-lg">{item.fullName}</h2>
                 <p className="text-gray-500">{item.treatment}</p>
-                <div className="flex items-center justify-center mt-2">
+                <div className="flex items-center justify-start mt-2">
                   <span className="text-accent mr-2 text-2xl">★</span>
                   <p className="text-sm font-medium">
                     {item.rating} ({item.reviews} reviews)
                   </p>
-                  </div>
                 </div>
               </div>
+            </div>
           </div>
         ))}
       </div>
