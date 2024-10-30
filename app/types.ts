1d91f087ad9c3b9d0a9d515e45d4afb4ef894080
herrbaha <baha_i@hotmail.com>
2024-10-30 11:06:35 +0100

    fix type

--- app/types.ts
+++ app/types.ts
@@ -1,50 +1,34 @@
-export interface ClinicDataTypes {
+interface BaseContentType {
   id: number;
-  type?: string;
+  title: string;
   image: string;
   alt: string;
-  title: string;
   rating?: number;
   reviews?: number;
 }
 
-export interface DoctorDataTypes {
-  id: number;
+export interface ClinicDataTypes extends BaseContentType {
   type?: string;
-  image: string;
-  alt: string;
+}
+
+export interface DoctorDataTypes extends BaseContentType {
   fullName: string;
   treatment: string;
-  rating?: number;
-  reviews?: number;
 }
 
-export interface BlogPostDataTypes {
-  id: number;
-  title: string;
-  image: string;
-  alt: string;
+export interface BlogPostDataTypes extends BaseContentType {
   date: string;
-  rating?: number;
-  reviews?: number;
 }
 
-export interface ButtonProps {
-  label: string | JSX.Element;
-  icon?: JSX.Element;
-  variation: string;
-}
-
-export interface DropdownProps {
-  placeholder: string;
-}
-
-export interface InputProps {
+interface BaseFormElementProps {
   value: any;
   onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
   label: string;
-  placeholder?: string;
   className?: string;
+}
+
+export interface InputProps extends BaseFormElementProps {
+  placeholder?: string;
   type?: "text" | "email" | "password" | "tel";
   pattern?: string;
   inputMode?:
@@ -58,36 +42,21 @@ export interface InputProps {
     | "decimal";
 }
 
-export interface CheckboxProps {
-  value: any;
-  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
-  label: string;
+export interface CheckboxProps extends BaseFormElementProps {
   checked: boolean;
-  className?: string;
-}
-
-export interface PatientFormState {
-  fullName: string;
-  email: string;
-  phone: string;
-  treatment: string;
-}
-
-export interface FacebookPictureData {
-  height: number;
-  is_silhouette: boolean;
-  url: string;
-  width: number;
-}
-
-export interface FacebookPicture {
-  data: FacebookPictureData;
 }
 
 export interface FacebookLoginResponse {
   name: string;
   email: string;
-  picture?: FacebookPicture | null;
+  picture?: {
+    data: {
+      height: number;
+      is_silhouette: boolean;
+      url: string;
+      width: number;
+    };
+  } | null;
   id: string;
   userID: string;
   expiresIn: number;
@@ -98,94 +67,13 @@ export interface FacebookLoginResponse {
   status?: string;
 }
 
-export interface ClinicDetailsTypes {
-  params: any;
+export interface ButtonProps {
+  label: string | JSX.Element;
+  icon?: JSX.Element;
+  variation: string;
 }
 
-export interface BreadCrumTypes {
-  clinicName: string;
-}
-
-export interface ProfileInfoProps {
-  onSubmit: (values: ProfileInfoValues) => void;
-}
-
-export interface ProfileInfoValues {
-  name: string;
-  email: string;
-  password: string;
-  phoneCode: string;
-  phoneNumber: string;
-  address: string;
-}
-
-export interface ClinicService {
-  name: string;
-  icon: JSX.Element;
-}
-
-export interface ClinicProps {
-  name: string;
-  rating: string;
-  reviews: number;
-  location: string;
-  treatments: string[];
-  services: ClinicService[];
-}
-
-export interface StarProps {
-  color: string;
-}
-
-export interface MapMarkerFCProps {
-  className?: string;
-}
-
-export interface ToggleSwitchProps {
-  className?: string;
-  checked: boolean;
-  onChange: () => void;
-}
-export interface SocialButtonProps {
+export interface SocialButtonProps extends ButtonProps {
   platform: "Google" | "Facebook" | "X";
   onClick: (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void;
-}
-export interface Comment {
-  title: string;
-  text: string;
-  name: string;
-  image: string;
-  reverse?: boolean;
-}
-export interface FooterLink {
-  label: string;
-  href: string;
-}
-export interface DashboardLocationProps {
-  mapLink: string;
-}
-
-export interface DescriptionType {
-  description: {
-    children: { text: string | { children?: { text: string }[] } }[];
-  }[];
-}
-
-export interface AccordionItemProps {
-  title: string;
-  children: React.ReactNode;
-  isOpen: boolean;
-  onToggle: () => void;
-}
-export interface Service {
-  id: number;
-  name: string;
-  icon: JSX.Element;
-  alt: string;
-}
-
-export interface PriceItemProps {
-  service: string;
-  minPrice: string;
-  maxPrice: string;
 }
